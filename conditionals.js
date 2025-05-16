0d53a4e2f62496f104c8617cb45f46fe9dc2b93a
Safal Shrestha <sshrestha24@wooster.edu>
2025-05-15 20:33:33 -0400

    conditional statements

--- conditionals.js
+++ conditionals.js
@@ -1,8 +1,23 @@
-var a = 5;
-var b = 10;
-console.log( a + b ); // 15
+// var a = 5;
+// var b = 10;
+// console.log( a + b ); // 15
 
-let age = 25;
-if (age >= 29) {
-    console.log("You are an adult.");
+let score = 90;
+console.log("You Scored " + score + " points.");
+
+// Conditional Statements
+if (score >= 90) {
+    console.log("Grade: A");
+}
+
+else if (score >= 80) {
+    console.log("Grade: B");
+}
+
+else if (score >= 70) {
+    console.log("Grade: C");
+}
+
+else {
+    console.log("Unfortunetely, you failed.");
 }
